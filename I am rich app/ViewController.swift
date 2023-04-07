92d103b8d77da566e3bf62147fed7f516e172e2a
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-04-06 21:43:49 -0300

    Initial Commit

---
+++ I am rich app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich app
+//
+//  Created by Orlando Moraes Martins on 06/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
